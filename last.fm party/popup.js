8e8dff2472e15fe9238ce425f513548c38f07c0e
Ofecka <chalupafilip@gmail.com>
2014-07-14 00:42:21 +0200

    Release 1.4.1

--- last.fm party/popup.js
+++ last.fm party/popup.js
@@ -5,7 +5,7 @@ _gaq.push(['_trackPageview']);
 
 (function() {
 	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = 'https://ssl.google-analytics.com/ga.js';
+	ga.src = 'https://stats.g.doubleclick.net/dc.js';
 	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
 //analytics end
